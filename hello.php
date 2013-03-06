2aba8db712326e87745d2634500a87448636093f
AsaKarlsson <asak@r-m.com>
2013-03-06 11:05:33 +0100

    Update hello.php

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
 <title>Hello Word</title>
 </head>
 <body>
-<h1>H</hl>
-<?php echo "My name is Mega"; ?>
+<h1>Hello, new version is upcoming</hl>
+<?php echo "My name is Mega Mic"; ?>
 </body>
 </html>
